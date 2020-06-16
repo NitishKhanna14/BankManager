0b829edb43bb7d93e35f525c8eefea00bcf0f4a8
root-ansh <anshsachdevaprofessional@gmail.com>
2020-06-16 19:54:09 +0530

    modified login logic

--- application/routes.py
+++ application/routes.py
@@ -2,30 +2,25 @@ from application import app # ,db
 from flask import render_template, request, json, Response, redirect, flash, url_for
 
 nav_details={
-    'login' : False
+    'login' : "abcdef"
 }
 
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/")
 def func_main():
     global nav_details
-    
+    return render_template("index.html", nav=nav_details)
 
-    if(nav_details['login']==True):
-        return render_template("index.html",nav=nav_details)
 
-    else:
-        return redirect("/login")
-
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def func_login():
+    return render_template("login.html",nav=nav_details)
+
+
+
+@app.route('/logout',methods=['POST'])
+def func_logout():
     global nav_details
+    nav_details['login']=""
+    return redirect('/')
 
-    if(nav_details['login']==True):
-        return redirect("index.html",nav=nav_details) 
-        #if login is true this means the word "logout" was showing to user
-        # which he/she clicked redirect to index with a post request to
-        #  logout the user 
-
-    else : 
-        return render_template("login.html",nav=nav_details)
